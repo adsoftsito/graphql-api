ed5186da4e44aaf28bfdfad75156921a477f345e
adsoftsito <adsoft@live.com.mx>
2021-07-19 22:29:34 +0000

    add create sale

--- sales/schema.py
+++ sales/schema.py
@@ -1,7 +1,25 @@
 import graphene
 from graphene_django import DjangoObjectType
 from .models import Sale
+from .models import Detail
 from users.schema import UserType
+#from graphene import ObjectType
+from graphene.types.generic import GenericScalar
+import json
+from django.core import serializers
+
+
+class DetailInput(graphene.InputObjectType):
+#    class Meta:
+#        model = Detail
+#        fields = ('product','cantidad','precio')
+    product : graphene.Int(required=True)
+    cantidad: graphene.Float(required=True)
+    precio: graphene.Float(required=True)
+
+class DetailType(DjangoObjectType):
+    class Meta:
+        model = Detail
 
 class SaleType(DjangoObjectType):
     class Meta:
@@ -19,15 +37,18 @@ class CreateSale(graphene.Mutation):
     iva = graphene.Float()
     total = graphene.Float()
     posted_by = graphene.Field(UserType)
+    #products = graphene.List(Detail)
 
     #2
     class Arguments:
         subtotal = graphene.Float()
         iva = graphene.Float()
         total = graphene.Float()
-
+        #products = graphene.List(DetailType)
+        products = GenericScalar() 
     #3
-    def mutate(self, info, subtotal, iva, total):
+    def mutate(self, info, subtotal, iva, total, products):
+
         user = info.context.user or None
 
         sale = Sale(
@@ -38,13 +59,28 @@ class CreateSale(graphene.Mutation):
             )
         sale.save()
 
+        myproducts = []
+        for product in products:        
+          myproduct = Detail(
+              product = product["product"],
+              cantidad = product["cantidad"],
+              precio = product["precio"],
+              sale = sale
+              )
+          myproduct.save()
+          myproducts.append(myproduct)
+          #print (product["product"])
+
+
+
         return CreateSale(
             id=sale.id,
             subtotal=sale.subtotal,
             iva=sale.iva,
             total=sale.total,
             posted_by=sale.posted_by
-        )
+           # products=myproducts
+            )
 
 #4
 class Mutation(graphene.ObjectType):
