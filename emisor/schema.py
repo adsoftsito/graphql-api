4fed8f696103d36a3c5b4640279fa1c2494b76f3
adsoftsito <adsoft@live.com.mx>
2021-07-27 22:02:46 +0000

    add emisor ok

--- emisor/schema.py
+++ emisor/schema.py
@@ -2,15 +2,16 @@ import graphene
 from graphene_django import DjangoObjectType
 from .models import Emisor
 from users.schema import UserType
+from django.db.models import Q
 
 class EmisorType(DjangoObjectType):
     class Meta:
         model = Emisor
 
 class Query(graphene.ObjectType):
-    emisors = graphene.List(EmisorType)
+    emisor = graphene.List(EmisorType)
 
-    def resolve_links(self, info, **kwargs):
+    def resolve_emisor(self, info, **kwargs):
         return Emisor.objects.all()
 
 class CreateEmisor(graphene.Mutation):
@@ -19,7 +20,7 @@ class CreateEmisor(graphene.Mutation):
     rfc = graphene.String()
     nombre = graphene.String()
     direccion = graphene.String()
-    cp = graphene.Int()
+    codigopostal = graphene.Int()
     regimenfiscal = graphene.String()
     certificado= graphene.String()
     filekey = graphene.String()
@@ -32,33 +33,45 @@ class CreateEmisor(graphene.Mutation):
       rfc = graphene.String()
       nombre = graphene.String()
       direccion = graphene.String()
-      cp = graphene.Int()
-      #regimenfiscal = graphene.String()
+      codigopostal = graphene.Int()
+      regimenfiscal = graphene.String()
       certificado = graphene.String()
       filekey = graphene.String()
       passcertificado = graphene.String()
 
 
     #3
-    def mutate(self, info, rfc, nombre, direccion, codigopostal, certificado, filekey, passcertificado):
+    def mutate(self, info, rfc, nombre, direccion, codigopostal, regimenfiscal, certificado, filekey, passcertificado):
         user = info.context.user or None
+        print(user)
+        currentEmisor = Emisor.objects.filter(posted_by=user).first()
+        print (currentEmisor)
 
         emisor = Emisor(
+            #id=currentEmisor.id,
             rfc=rfc, 
             nombre=nombre,
             direccion=direccion,
             cp=codigopostal,
+            regimenfiscal=regimenfiscal,
             certificado=certificado,
             filekey=filekey,
             passcertificado=passcertificado,
             posted_by = user
             )
+
+        if currentEmisor:
+            emisor.id = currentEmisor.id
+
         emisor.save()
 
         return CreateEmisor(
             id=emisor.id,
             rfc=emisor.rfc,
+            nombre=emisor.nombre,
             direccion=emisor.direccion,
+            codigopostal=emisor.cp,
+            regimenfiscal=emisor.regimenfiscal,
             posted_by=emisor.posted_by
         )
 
