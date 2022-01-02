50120213506efb71f4e7343cc45ccd212305af22
adsoftsito <adsoft@live.com.mx>
2022-01-02 04:23:09 +0000

    update estaciones

--- cats/models.py
+++ cats/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 class Estacion(models.Model):
     clave = models.TextField(blank=True)
-    description = models.TextField(blank=True)
-    clave_transporte = models.TextField(blank=True)
-    nacionalidad = models.TextField(blank=True)
-    designador_iata = models.TextField(blank=True)
-    linea_ferrea = models.TextField(blank=True)
+    descripcion = models.TextField(blank=True)
+    clave_transporte = models.TextField(blank=True, null=True)
+    nacionalidad = models.TextField(blank=True, null=True)
+    designador_iata = models.TextField(blank=True, null=True)
+    linea_ferrea = models.TextField(blank=True, null=True)
 
