d6f676c63fc79c47386f828bf2cb9069ce675acc
adsoftsito <adsoft@live.com.mx>
2022-10-09 20:40:43 -0500

    Update urls.py

--- hackernews/urls.py
+++ hackernews/urls.py
@@ -22,4 +22,5 @@ from graphene_django.views import GraphQLView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
+    url(r'^ht/', include('health_check.urls')),
 ]
